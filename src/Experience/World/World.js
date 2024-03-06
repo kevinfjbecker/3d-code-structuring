b501e776e67f42bdf63b0be71a53cf96d2cdf4c9
Kevin Becker <kevinfjbecker@gmail.com>
2024-03-06 22:23:17 +0100

    Floor

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 
 import Experience from '../Experience.js'
 import Environment from './Environment.js'
+import Floor from './Floor.js'
 
 export default class World
 {
@@ -10,18 +11,13 @@ export default class World
         this.experience = new Experience()
         this.scene = this.experience.scene
 
-        const testMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshStandardMaterial()
-        )
-        this.scene.add(testMesh)
-
         this.resources = this.experience.resources
 
         // Wait for resources
         this.resources.on('ready', () =>
         {
             // Setup
+            this.floor = new Floor()
             this.experience = new Environment()
         })
     }
